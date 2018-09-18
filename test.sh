324021a1ea1b22df6b0080254fc473b5496eecab
szrlee <szrlee@gmail.com>
2018-09-18 12:30:00 +0000

    ignore training and testing scriptOO

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 now=$(date +"%Y%m%d_%H%M%S")
-python test_net.py --dataset pascal_voc --net vgg16 \
-                   --checksession 6 --checkepoch 6 --checkpoint 2504 \
+python test_net.py --dataset pascal_voc_0712 --net vgg16 \
+                   --checksession 7 --checkepoch 24 --checkpoint 8274 \
                    --cuda \
-                   2>&1|tee ./log/pretrained/test/vgg16-$now.log 
+                   2>&1|tee ./log/pretrained/test/vgg16-VOC0712-$now.log 
--- train.sh
+++ train.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 now=$(date +"%Y%m%d_%H%M%S")
-python trainval_net.py --dataset pascal_voc --net vgg16 \
-                       --bs 4 --nw 2 --epochs 18\
+python trainval_net.py --dataset pascal_voc_0712 --net vgg16 \
+                       --bs 4 --nw 2 --epochs 30 \
                        --lr 0.0025 --lr_decay_step 6 --lr_decay_gamma 0.1 \
                        --cuda \
-                       --s 6 \
-                       2>&1|tee ./log/pretrained/vgg16-$now.log 
+                       --s 7 \
+                       --r true --checksession 7 --checkepoch 18 --checkpoint 8274
+                       2>&1|tee ./log/pretrained/vgg16-VOC0712-$now.log 
