3440a04f16f55c08b873e603e5cb328929728ff3
szrlee <szrlee@gmail.com>
2018-09-23 18:20:30 +0000

    modify script

--- test_dt_comic.sh
+++ test_dt_comic.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 now=$(date +"%Y%m%d_%H%M%S")
 python test_net.py --dataset pascal_voc_comic_0712 --net vgg16 \
-                   --checksession 103 --checkepoch 18 --checkpoint 7019 \
+                   --checksession 103 --checkepoch $1 --checkpoint 7019 \
                    --cuda \
                    2>&1|tee ./log/dt_comic/test/vgg16-$now.log 
--- train_dt_watercolor.sh
+++ train_dt_watercolor.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 now=$(date +"%Y%m%d_%H%M%S")
 python trainval_net.py --dataset pascal_voc_watercolor_0712 --net vgg16 \
-                       --bs 4 --nw 2 --epochs 30 \
-                       --lr 0.0025 --lr_decay_step 6 --lr_decay_gamma 0.1 \
+                       --bs 4 --nw 2 --epochs 60 \
+                       --lr 0.0001 --lr_decay_step 20 --lr_decay_gamma 0.5 \
                        --cuda \
-                       --s 104 \
+                       --s 107 \
                        --dt true --pretrained_rcnn data/pretrained_rcnn/vgg16-VOC0712.pth \
                        2>&1|tee ./log/dt_watercolor/vgg16-$now.log 
