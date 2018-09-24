4ec18611db9d7e3598f42e42bb69932c53caf509
Richard <szrlee@gmail.com>
2018-09-24 10:10:00 +0800

    modity script

--- test_baseline.sh
+++ test_baseline.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 now=$(date +"%Y%m%d_%H%M%S")
+echo "clipart"
 ./test_pretrained_baseline.sh clipart
-
+echo "comic"
 ./test_pretrained_baseline.sh comic
-
+echo "watercolor"
 ./test_pretrained_baseline.sh watercolor
 
 echo $now
--- test_dt.sh
+++ test_dt.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 now=$(date +"%Y%m%d_%H%M%S")
+echo "clipart"
 ./test_pretrained_dt.sh clipart
-
+echo "comic"
 ./test_pretrained_dt.sh comic
-
+echo "watercolor"
 ./test_pretrained_dt.sh watercolor
 
 echo $now
