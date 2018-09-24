aa12848d6c9a3f4a79a1dbcaaaa93bb0bcdb8372
Richard <szrlee@gmail.com>
2018-09-24 10:01:23 +0800

    modify

--- test_pretrained_baseline.sh
+++ test_pretrained_baseline.sh
@@ -3,4 +3,4 @@ now=$(date +"%Y%m%d_%H%M%S")
 python test_net.py --dataset $1 --net vgg16 \
                    --dt true --pretrained_rcnn data/pretrained_rcnn/vgg16-VOC0712.pth \
                    --cuda \
-                   2>&1|tee ./log/baseline_test/$1-vgg16-$now.log 
+                   2>&1|tee ./log/test_pretrained_baseline/$1-vgg16-$now.log 
--- test_pretrained_dt.sh
+++ test_pretrained_dt.sh
@@ -3,4 +3,4 @@ now=$(date +"%Y%m%d_%H%M%S")
 python test_net.py --dataset $1 --net vgg16 \
                    --dt true --pretrained_rcnn data/pretrained_rcnn/dt_$1/vgg16-VOC0712-VOC0712_$1.pth \
                    --cuda \
-                   2>&1|tee ./log/dt_test/$1-vgg16-$now.log 
+                   2>&1|tee ./log/test_pretrained_dt/$1-vgg16-$now.log 
